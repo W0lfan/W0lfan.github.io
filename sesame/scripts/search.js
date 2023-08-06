a1ca272f6f82cd5e5cc04f79c4f0e5a7f7085f52
naflouille <69418024+W0lfan@users.noreply.github.com>
2023-08-06 18:07:51 +0200

    Update search.js

--- sesame/scripts/search.js
+++ sesame/scripts/search.js
@@ -84,7 +84,7 @@ function QueryItems(r) {
             }
 
     }
-    fetch("/modding/database/mods.json")
+    fetch("/database/mods.json")
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -101,7 +101,7 @@ function QueryItems(r) {
       console.error('Fetch error:', error);
     });
 
-    fetch("/modding/database/codes.json")
+    fetch("/database/codes.json")
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok');
