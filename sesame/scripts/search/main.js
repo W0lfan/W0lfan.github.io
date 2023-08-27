26eb5e9820b35ae2c503b2f2fe50dd9176a7b5f0
naflouille <naflouille@proton.me>
2023-08-27 18:17:46 +0200

    Update main.js

--- sesame/scripts/search/main.js
+++ sesame/scripts/search/main.js
@@ -734,9 +734,9 @@ function Search(search_query = null, official_content = false, all = "", not_que
                                                         }
                                                         ${
                                                             content.open ? `
-                                                            <div class="action" id="${content.name.replace(/ /g, '-')}-code">
+                                                            <div class="action" onclick="window.open('${content.open}')">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"/></svg>
-                                                                <div class="text">Open</div>
+                                                                <div class="text">${LanguageValues.actions.open}</div>
                                                             </div>
                                                             ` : ''
                                                         }
