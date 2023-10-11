42642a60107df730cf744fa4278397faaa4a9435
naflouille <naflouille@proton.me>
2023-10-11 16:54:55 +0200

    Update calendar.js

--- sesame/planning/scripts/calendar.js
+++ sesame/planning/scripts/calendar.js
@@ -52,12 +52,6 @@ const __CALENDAR__ = [
                     description : "Better readability of the source code"
                 },
                 svg : '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M560-160v-80h120q17 0 28.5-11.5T720-280v-80q0-38 22-69t58-44v-14q-36-13-58-44t-22-69v-80q0-17-11.5-28.5T680-720H560v-80h120q50 0 85 35t35 85v80q0 17 11.5 28.5T840-560h40v160h-40q-17 0-28.5 11.5T800-360v80q0 50-35 85t-85 35H560Zm-280 0q-50 0-85-35t-35-85v-80q0-17-11.5-28.5T120-400H80v-160h40q17 0 28.5-11.5T160-600v-80q0-50 35-85t85-35h120v80H280q-17 0-28.5 11.5T240-680v80q0 38-22 69t-58 44v14q36 13 58 44t22 69v80q0 17 11.5 28.5T280-240h120v80H280Z"/></svg>'
-            }, {
-                txt : {
-                    title : "Smart tools",
-                    description : "Introducing a various range of powerful tools for modders and users"
-                },
-                svg : '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M551-80 406-392 240-160v-720l560 440H516l144 309-109 51Z"/></svg>'
             }
         ]
     },
@@ -65,7 +59,7 @@ const __CALENDAR__ = [
         date : 'Jan. 2024 - Feb. 2024',
         finished : false,
         title : 'SesMarTools update',
-        description : "Sesame Smart Tools, or the SesMarTools update, will bring to users and coders a whole new exerience, enhancing their naviguation through smart datas indexing systems.",
+        description : "Sesame Smart Tools, or the SesMarTools update, will bring to users and coders a whole new exerience, enhancing their naviguation through smart datas indexing systems",
         content : [
              {
                 txt : {
