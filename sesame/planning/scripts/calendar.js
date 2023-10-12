db1ab9f5363901c8b5abdf51a4174e508198316e
naflouille <naflouille@proton.me>
2023-10-12 08:13:23 +0200

    Update calendar.js

--- sesame/planning/scripts/calendar.js
+++ sesame/planning/scripts/calendar.js
@@ -15,12 +15,12 @@ const __CALENDAR__ = [
         date : 'Oct. 2023 - Nov. 2023',
         finished : false,
         title : "Sesame Administration",
-        description : "A better way for administrators to manage your datas",
+        description : "A better way for administrators to manage your data",
         content : [
             {
                 txt : {
                     title : "Sesadmin",
-                    description : "A tool for the administrators to manage the datas from the web"
+                    description : "A tool for the administrators to manage the data from the web"
                 },
                 svg : '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-440q58 0 99-41t41-99q0-58-41-99t-99-41q-58 0-99 41t-41 99q0 58 41 99t99 41Zm0 360q-146-37-233-160t-87-276v-244l320-120 320 120v244q0 153-87 276T480-80Zm0-84q59-19 104.5-59.5T664-315q-43-22-89.5-33.5T480-360q-48 0-94.5 11.5T296-315q34 51 79.5 91.5T480-164Z"/></svg>'
             },
@@ -52,6 +52,13 @@ const __CALENDAR__ = [
                     description : "Better readability of the source code"
                 },
                 svg : '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M560-160v-80h120q17 0 28.5-11.5T720-280v-80q0-38 22-69t58-44v-14q-36-13-58-44t-22-69v-80q0-17-11.5-28.5T680-720H560v-80h120q50 0 85 35t35 85v80q0 17 11.5 28.5T840-560h40v160h-40q-17 0-28.5 11.5T800-360v80q0 50-35 85t-85 35H560Zm-280 0q-50 0-85-35t-35-85v-80q0-17-11.5-28.5T120-400H80v-160h40q17 0 28.5-11.5T160-600v-80q0-50 35-85t85-35h120v80H280q-17 0-28.5 11.5T240-680v80q0 38-22 69t-58 44v14q36 13 58 44t22 69v80q0 17 11.5 28.5T280-240h120v80H280Z"/></svg>'
+            },
+            {
+                txt : {
+                    title : "Sesame rebranding",
+                    description : "Sesame Logo and visuals rebranded"
+                },
+                svg : '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M240-120q-45 0-89-22t-71-58q26 0 53-20.5t27-59.5q0-50 35-85t85-35q50 0 85 35t35 85q0 66-47 113t-113 47Zm230-240L360-470l358-358q11-11 27.5-11.5T774-828l54 54q12 12 12 28t-12 28L470-360Z"/></svg>'
             }
         ]
     },
@@ -70,8 +77,8 @@ const __CALENDAR__ = [
             },
             {
                 txt : {
-                    title : "Datas empowering",
-                    description : "Sesame database will grow stronger, allowing people to access a more powerful range of datas."
+                    title : "Data empowering",
+                    description : "Sesame database will grow stronger, allowing people to access a more powerful range of data."
                 },
                 svg : '<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q146 0 255.5 91.5T872-559h-82q-19-73-68.5-130.5T600-776v16q0 33-23.5 56.5T520-680h-80v80q0 17-11.5 28.5T400-560h-80v80h80v120h-40L168-552q-3 18-5.5 36t-2.5 36q0 131 92 225t228 95v80Zm364-20L716-228q-21 12-45 20t-51 8q-75 0-127.5-52.5T440-380q0-75 52.5-127.5T620-560q75 0 127.5 52.5T800-380q0 27-8 51t-20 45l128 128-56 56ZM620-280q42 0 71-29t29-71q0-42-29-71t-71-29q-42 0-71 29t-29 71q0 42 29 71t71 29Z"/></svg>'
             }
